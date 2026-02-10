93046fef87fc9424a55ad3bc46dd569c5604f856
Florian Chanson <fchanson@solem.fr>
2026-02-10 10:14:17 +0100

    Camera rats

--- src/main/kotlin/dev/blackoutburst/bogel/camera/Camera.kt
+++ src/main/kotlin/dev/blackoutburst/bogel/camera/Camera.kt
@@ -4,37 +4,61 @@ import dev.blackoutburst.bogel.input.Mouse
 import dev.blackoutburst.bogel.maths.Matrix
 import dev.blackoutburst.bogel.maths.Vector2f
 import dev.blackoutburst.bogel.maths.Vector3f
+import dev.blackoutburst.bogel.maths.Vector4f
 import dev.blackoutburst.bogel.window.Window
 
 object Camera {
-    private var lastMousePosition = Mouse.position
-
-    var position = Vector3f(0f, 0f, 5f)
-
-    var rotation = Vector2f(40f, 30f)
+    var position = Vector3f(0f, 0f, 0f)
+    var positionOffset = Vector3f(0f, 0f, 0f)
+    var rotation = Vector2f(0f, 0f)
 
     var view = Matrix().translate(position)
     var projection = Matrix().projectionMatrix(90f, 1000f, 0.1f)
     var projection2D = Matrix().ortho2D(0f, Window.width.toFloat(), 0f, Window.height.toFloat(), -1f, 1f)
 
-    fun update() {
-        val mousePosition = Mouse.position
+    val ray: Vector3f
+        get() {
+            val mouseXNDC = (2.0 * Mouse.position.x / Window.width) - 1.0
+            val mouseYNDC = 1.0 - (2.0 * Mouse.position.y / Window.height)
+            val rayClip = Vector4f(mouseXNDC.toFloat(), mouseYNDC.toFloat(), -1.0f, 1.0f)
 
-        var xOffset = 0f
-        var yOffset = 0f
+            val inverseProjection = projection.copy().invert()
+            val inverseView = view.copy().invert()
 
-        if (Mouse.isButtonDown(Mouse.MIDDLE_BUTTON)) {
-            xOffset = mousePosition.x - lastMousePosition.x
-            yOffset = mousePosition.y - lastMousePosition.y
+            val rayEye = inverseProjection.transform(rayClip)
+            rayEye.z = -1.0f
+            rayEye.w = 0.0f
+
+            val rayWorld = inverseView.transform(Vector4f(rayEye.x, rayEye.y, rayEye.z, rayEye.w))
+
+            return Vector3f(rayWorld.x, rayWorld.y, rayWorld.z).normalize()
         }
 
-        lastMousePosition = mousePosition.copy()
+    private fun getSpacePosition(): Vector3f {
+        val inverseView = view.copy().invert()
+        return Vector3f(inverseView.m30, inverseView.m31, inverseView.m32)
+    }
 
-        //position.z -= Mouse.scroll / 2f
-        position.x += xOffset
-        position.y -= yOffset
+    fun getWorldPositionAtY0(): Vector3f? {
+        val rayOrigin = getSpacePosition()
+        val rayDirection = ray
 
-        view.setIdentity()
-            .translate(position.x, position.y, 0f)
+        if (rayDirection.y == 0f) {
+            return null
+        }
+
+        val t = -rayOrigin.y / rayDirection.y
+
+        if (t < 0f) {
+            return null
+        }
+
+        val intersectionPoint = Vector3f(
+            rayOrigin.x + rayDirection.x * t,
+            0f,
+            rayOrigin.z + rayDirection.z * t
+        )
+
+        return intersectionPoint
     }
 }
