d8ad816e468a8e78a1d34fb394728b205a29587a
Florian Chanson <fchanson@solem.fr>
2026-02-11 11:02:29 +0100

    cross platform uber jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,28 +14,20 @@ version = "1.0-SNAPSHOT"
 
 val lwjglVersion = "3.4.1"
 
-val lwjglNatives = Pair(
-    System.getProperty("os.name")!!,
-    System.getProperty("os.arch")!!
-).let { (name, arch) ->
-    when {
-        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
-            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
-                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
-            else if (arch.startsWith("ppc"))
-                "natives-linux-ppc64le"
-            else if (arch.startsWith("riscv"))
-                "natives-linux-riscv64"
-            else
-                "natives-linux"
-        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }     ->
-            "natives-macos-arm64"
-        arrayOf("Windows").any { name.startsWith(it) }                ->
-            "natives-windows"
-        else                                                                            ->
-            throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
-    }
-}
+val lwjglModules = listOf("lwjgl", "lwjgl-assimp", "lwjgl-glfw", "lwjgl-opengl", "lwjgl-stb")
+
+val nativesClassifiers = listOf(
+    "natives-linux",
+    "natives-linux-arm32",
+    "natives-linux-arm64",
+    "natives-linux-ppc64le",
+    "natives-linux-riscv64",
+    "natives-macos",
+    "natives-macos-arm64",
+    "natives-windows",
+    "natives-windows-x86",
+    "natives-windows-arm64"
+)
 
 repositories {
     mavenCentral()
@@ -44,16 +36,15 @@ repositories {
 dependencies {
     implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
 
-    implementation("org.lwjgl", "lwjgl")
-    implementation("org.lwjgl", "lwjgl-assimp")
-    implementation("org.lwjgl", "lwjgl-glfw")
-    implementation("org.lwjgl", "lwjgl-opengl")
-    implementation("org.lwjgl", "lwjgl-stb")
-    implementation ("org.lwjgl", "lwjgl", classifier = lwjglNatives)
-    implementation ("org.lwjgl", "lwjgl-assimp", classifier = lwjglNatives)
-    implementation ("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
-    implementation ("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
-    implementation ("org.lwjgl", "lwjgl-stb", classifier = lwjglNatives)
+    lwjglModules.forEach { module ->
+        implementation("org.lwjgl:$module")
+    }
+
+    lwjglModules.forEach { module ->
+        nativesClassifiers.forEach { classifier ->
+            implementation("org.lwjgl:$module:$lwjglVersion:$classifier")
+        }
+    }
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
 }
