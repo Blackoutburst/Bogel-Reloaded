be9c9263a5b1354975b210e9808f1252cf8317b3
Florian Chanson <fchanson@solem.fr>
2026-02-11 10:56:47 +0100

    fixed macos shenanigans

--- src/main/kotlin/dev/blackoutburst/bogel/window/callbacks/MousePositionCallBack.kt
+++ src/main/kotlin/dev/blackoutburst/bogel/window/callbacks/MousePositionCallBack.kt
@@ -4,16 +4,18 @@ import dev.blackoutburst.bogel.input.Mouse
 import dev.blackoutburst.bogel.utils.stack
 import org.lwjgl.glfw.GLFW.glfwGetCursorPos
 import org.lwjgl.glfw.GLFWCursorPosCallbackI
+import org.lwjgl.system.Platform
 
 class MousePositionCallBack : GLFWCursorPosCallbackI {
     override fun invoke(window: Long, xPos: Double, yPos: Double) {
         stack {
+            val scale = if (Platform.get() == Platform.MACOSX) 2.0 else 1.0
             val width = it.mallocDouble(1)
             val height = it.mallocDouble(1)
 
             glfwGetCursorPos(window, width, height)
 
-            Mouse.position.set(width.get().toFloat(), height.get().toFloat())
+            Mouse.position.set((width.get() * scale).toFloat(), (height.get() * scale).toFloat())
         }
     }
 }
--- src/main/kotlin/dev/blackoutburst/bogel/window/callbacks/WindowCallBack.kt
+++ src/main/kotlin/dev/blackoutburst/bogel/window/callbacks/WindowCallBack.kt
@@ -5,12 +5,15 @@ import dev.blackoutburst.bogel.maths.Matrix
 import org.lwjgl.glfw.GLFW.glfwSetWindowSize
 import org.lwjgl.glfw.GLFWWindowSizeCallbackI
 import org.lwjgl.opengl.GL11.glViewport
+import org.lwjgl.system.Platform
 
 class WindowCallBack : GLFWWindowSizeCallbackI {
     override fun invoke(window: Long, width: Int, height: Int) {
-        glfwSetWindowSize(window, width, height)
-        glViewport(0, 0, width, height)
+        val scale = if (Platform.get() == Platform.MACOSX) 2 else 1
 
-        Camera.projection2D = Matrix().ortho2D(0f, width.toFloat(), 0f, height.toFloat(), -1f, 1f)
+        glfwSetWindowSize(window, width, height)
+        glViewport(0, 0, width * scale, height * scale)
+
+        Camera.projection2D = Matrix().ortho2D(0f, (width * scale).toFloat(), 0f, (height * scale).toFloat(), -1f, 1f)
     }
 }
